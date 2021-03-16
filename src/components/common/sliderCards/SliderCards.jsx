2d4b9b1eba734fb26628e2e00ce0ba8f5be3b91e
Mila8web <mila8web@gmail.com>
2021-03-16 21:59:50 +0300

    slider

--- src/components/common/sliderCards/SliderCards.jsx
+++ src/components/common/sliderCards/SliderCards.jsx
@@ -5,6 +5,11 @@ import recordPlayer from "./../../../assets/images/svg/recordPlayer.svg"
 import navigator from "./../../../assets/images/svg/navigator.svg"
 import RF from "./../../../assets/images/svg/RF.svg"
 import suit from "./../../../assets/images/svg/suit.svg"
+import bag from "./../../../assets/images/svg/bag9800.svg"
+import jacket from "./../../../assets/images/svg/jacket6900.svg"
+import BRP from "./../../../assets/images/svg/BRP68000.svg"
+import equipment from "./../../../assets/images/svg/equipment.svg"
+
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation,Autoplay  } from 'swiper';
@@ -31,8 +36,8 @@ const SliderCards = (props) => {
 				onSlideChange={() => console.log('slide change')}
 				onSwiper={(swiper) => console.log(swiper)}
 			>
-				<SwiperSlide className="swipper">
-					<Card
+				<SwiperSlide className={s.swipper}>
+					<Card className={s.card}
 						image={recordPlayer}
 						name='BRP Audio-портативная система'
 						price=''
@@ -41,8 +46,8 @@ const SliderCards = (props) => {
 					/>
 				</SwiperSlide>
 
-				<SwiperSlide className="swipper">
-					<Card
+				<SwiperSlide className={s.swipper}>
+					<Card className={s.card}
 						image={navigator}
 						name='Garmin Echomap Plus 62cv'
 						price='7 000 ₽'
@@ -51,8 +56,8 @@ const SliderCards = (props) => {
 					/>
 				</SwiperSlide>
 
-				<SwiperSlide className="swipper">
-					<Card
+				<SwiperSlide className={s.swipper}>
+					<Card className={s.card}
 						image={RF}
 						name='RF D.E.S.S.TM Key'
 						price=''
@@ -61,8 +66,8 @@ const SliderCards = (props) => {
 					/>
 				</SwiperSlide>
 
-				<SwiperSlide className="swipper">
-					<Card
+				<SwiperSlide className={s.swipper}>
+					<Card className={s.card}
 						image={suit}
 						name='Мужской костюм 3мм'
 						price='7 000 ₽'
@@ -71,6 +76,45 @@ const SliderCards = (props) => {
 					/>
 				</SwiperSlide>
 
+				<SwiperSlide className={s.swipper}>
+					<Card className={s.card}
+						image={bag}
+						name='Водонепроницаемый Рюкзак'
+						price='9 800 ₽'
+						absent=''
+						message=''
+					/>
+				</SwiperSlide>
+
+				<SwiperSlide className={s.swipper}>
+					<Card className={s.card}
+						image={jacket}
+						name="Спасательный жилет BRP Men's Airflow PFD"
+						price='6 900 ₽'
+						absent=''
+						message=''
+					/>
+				</SwiperSlide>
+
+				<SwiperSlide className={s.swipper}>
+					<Card className={s.card}
+						image={BRP}
+						name='BRP Audio-Premium System'
+						price='68 000 ₽'
+						absent=''
+						message=''
+					/>
+				</SwiperSlide>
+
+				<SwiperSlide className={s.swipper}>
+					<Card className={s.card}
+						image={equipment}
+						name='Спасательное снаряжение'
+						price=''
+						absent='нет в наличии'
+						message='Сообщить о поступлении'
+					/>
+				</SwiperSlide>
 				
 			</Swiper>
 		</div>
