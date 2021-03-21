bf597260952afca19d261235cfbd17642132e77d
Mila8web <mila8web@gmail.com>
2021-03-21 17:33:20 +0300

    PageProduct

--- src/components/block/navigation/data.jsx
+++ src/components/block/navigation/data.jsx
@@ -1,16 +1,24 @@
-function Data(text,href) {
+function Data(text, href) {
 	this.text = text
 	this.href = href
 }
 
 export const data = [
-	new Data('Квадроциклы','/PageCatalog'),
-	new Data('Гидроциклы', '/PageCatalog'),
-	new Data('Катера',     '/PageCatalog'),
-	new Data('Вездеходы',  '/PageCatalog'),
-	new Data('Снегоходы',  '/PageCatalog'),
-	new Data('Двигатели',  '/PageCatalog'),
-	new Data('Запчасти',   '/PageCatalog'),
+	new Data('Квадроциклы', './1'),
+	new Data('Гидроциклы', './2'),
+	new Data('Катера', './3'),
+	new Data('Вездеходы', './4'),
+	new Data('Снегоходы', './5'),
+	new Data('Двигатели', './6'),
+	new Data('Запчасти', './7'),
+	new Data('О товаре', './8'),
+
+	new Data('Характеристики', './9'),
+	new Data('Отзывы', './10'),
+	new Data('Самовывоз', './11'),
+	new Data('Доставка', './12'),
+	new Data('Cервис', './13'),
+	new Data('Гарантия', './14'),
 ]
 
 
