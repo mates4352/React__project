c62436c385f2b6dfd253b7d63bc4b4da8f0e07d5
Mila8web <mila8web@gmail.com>
2021-03-23 21:33:07 +0300

    SliderCards

--- src/components/common/card/data.jsx
+++ src/components/common/card/data.jsx
@@ -1,10 +1,40 @@
+// Картинки из PageProduct 
+// import recordPlayer from "./../../../assets/images/svg/recordPlayer.svg"
+// import navigator from "./../../../assets/images/svg/navigator.svg"
+// import RF from "./../../../assets/images/svg/RF.svg"
+// import suit from "./../../../assets/images/svg/suit.svg"
+// import bag from "./../../../assets/images/svg/bag9800.svg"
 
-function Data(sale) {
-	this.sale = sale
-}
 
-export const data = [
-	new Data("asdf"),
+// function Data(image, name, price, absent, message) {
+// 	this.image = image
+// 	this.name = name
+// 	this.price = price
+// 	this.absent = absent
+// 	this.message = message
+// }
 
-]
 
+// export const data = [
+// 	new Data(recordPlayer,'BRP Audio-портативная система','','нет в наличии','Сообщить о поступлении'),
+// 	new Data(navigator,'Garmin Echomap Plus 62cv','45 800 ₽','',''),
+// 	new Data(RF,'RF D.E.S.S.TM Key','','нет в наличии','Сообщить о поступлении'),
+// 	new Data(suit,'Мужской костюм 3мм','7 000 ₽','',''),
+// 	new Data(bag,'BRP Audio-портативная система','','нет в наличии','Сообщить о поступлении')
+// ]
+
+
+// const data0 = {
+// 		image: recordPlayer,
+// 		name:'BRP Audio-портативная система',
+// 		price:'',
+// 		absent:'нет в наличии',
+// 		message:'Сообщить о поступлении'
+// 	}
+
+// 	const data = [
+// 		data0
+		
+// 	]
+	
+// 	export default data
--- src/components/common/sliderCards/SliderCards.jsx
+++ src/components/common/sliderCards/SliderCards.jsx
@@ -6,9 +6,9 @@ import navigator from "./../../../assets/images/svg/navigator.svg"
 import RF from "./../../../assets/images/svg/RF.svg"
 import suit from "./../../../assets/images/svg/suit.svg"
 import bag from "./../../../assets/images/svg/bag9800.svg"
-import jacket from "./../../../assets/images/svg/jacket6900.svg"
-import BRP from "./../../../assets/images/svg/BRP68000.svg"
-import equipment from "./../../../assets/images/svg/equipment.svg"
+// import jacket from "./../../../assets/images/svg/jacket6900.svg"
+// import BRP from "./../../../assets/images/svg/BRP68000.svg"
+// import equipment from "./../../../assets/images/svg/equipment.svg"
 
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -23,6 +23,59 @@ SwiperCore.use([Navigation,Autoplay,]);
 
 
 const SliderCards = (props) => {
+	// const data0 = {
+	// 	image: recordPlayer,
+	// 	name:'BRP Audio-портативная система',
+	// 	price:'',
+	// 	absent:'нет в наличии',
+	// 	message:'Сообщить о поступлении',
+	// 	// link: '/1'
+	// }
+	
+	// const data1 = {
+	// 	image: navigator,
+	// 	name:'Garmin Echomap Plus 62cv',
+	// 	price:'7 000 ₽',
+	// 	absent:'',
+	// 	message:'',
+	// 	// link: '/2'
+	// }
+	
+	// const data2 = {
+	// 	image: RF,
+	// 	name:'RF D.E.S.S.TM Key',
+	// 	price:'',
+	// 	absent:'нет в наличии',
+	// 	message:'Сообщить о поступлении',
+	// 	// link: '/3'
+	// }
+	
+	// const data3 = {
+	// 	image: suit,
+	// 	name:'Мужской костюм 3мм',
+	// 	price:'7 000 ₽',
+	// 	absent:'',
+	// 	message:'',
+	// 	// link: '/4'
+	// }
+	
+	// const data4 = {
+	// 	image: bag,
+	// 	name:'Водонепроницаемый Рюкзак',
+	// 	price:'9 800 ₽',
+	// 	absent:'',
+	// 	message:'',
+	// 	// link: '/5'
+	// }
+
+	let data = [
+		{ image: recordPlayer, name: 'BRP Audio-портативная система', price: '', absent: 'нет в наличии', message: 'Сообщить о поступлении'},
+		{ image: navigator, name: 'Garmin Echomap Plus 62cv', price: '7 000 ₽', absent: '', message: ''},
+		{ image: RF, name: 'RF D.E.S.S.TM Key', price: '', absent: 'нет в наличии', message: 'Сообщить о поступлении'},
+		{ image: suit, name: 'Мужской костюм 3мм', price: '7 000 ₽', absent: '', message: ''},
+		{ image: bag, name: 'Водонепроницаемый Рюкзак', price: '9 800 ₽', absent: '', message: ''}
+	]
+
 	return (
 		<div className="wrapper">
 			<Swiper
@@ -38,52 +91,59 @@ const SliderCards = (props) => {
 				onSwiper={(swiper) => console.log(swiper)}
 			>
 				<SwiperSlide className="swipper">
+				{/* {props.data0 && */}
 					<Card className="card"
-						image={recordPlayer}
-						name='BRP Audio-портативная система'
-						price=''
-						absent='нет в наличии'
-						message='Сообщить о поступлении'
+						image={data[0].image}
+						name={data[0].name}
+						price={data[0].price}
+						absent={data[0].absent}
+						message={data[0].message}
 					/>
 				</SwiperSlide>
 
 				<SwiperSlide className="swipper">
+				{/* {props.data1 && */}
 					<Card className="card"
-						image={navigator}
-						name='Garmin Echomap Plus 62cv'
-						price='7 000 ₽'
-						absent=''
-						message=''
+						image={data[1].image}
+						name={data[1].name}
+						price={data[1].price}
+						absent={data[1].absent}
+						message={data[1].message}
 					/>
 				</SwiperSlide>
 
 				<SwiperSlide className="swipper">
+				{/* {props.data2 && */}
 					<Card className="card"
-						image={RF}
-						name='RF D.E.S.S.TM Key'
-						price=''
-						absent='нет в наличии'
-						message='Сообщить о поступлении'
+					image={data[2].image}
+					name={data[2].name}
+					price={data[2].price}
+					absent={data[2].absent}
+					message={data[2].message}
 					/>
 				</SwiperSlide>
 
 				<SwiperSlide className="swipper">
+				{/* {props.data3 && */}
 					<Card className="card"
-						image={suit}
-						name='Мужской костюм 3мм'
-						price='7 000 ₽'
-						absent=''
-						message=''
+						image={data[3].image}
+						name={data[3].name}
+						price={data[3].price}
+						absent={data[3].absent}
+						message={data[3].message}
 					/>
 				</SwiperSlide>
 
 				<SwiperSlide className="swipper">
+				{/* {props.data4 && */}
 					<Card className="card"
-						image={bag}
-						name='Водонепроницаемый Рюкзак'
-						price='9 800 ₽'
-						absent=''
-						message=''
+					image={data[4].image}
+					name={data[4].name}
+					price={data[4].price}
+					absent={data[4].absent}
+					message={data[4].message}
+					// link={data4.link}
+	
 					/>
 				</SwiperSlide>
 				
